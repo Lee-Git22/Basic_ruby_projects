3a1d4d585f561a67c761c68d017ffa306ebd11f3
Alan <lee.alan9801@gmail.com>
2023-01-13 12:09:55 -0500

    Implement substrings

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ This is a collection of basic programming projects written Ruby from The Odin Pr
 Caesar cipher: Implement a method that takes in a string message and a shift factor and outputs an encrypted message
   - Added a feature to allow user to enter their own message and shift factor throught the CLI
   - Added checks to ensure a valid shift factor
+
+Substrings: Implement a method that takes in a string and a dictionary (an array of valid substrings) and return a hash listing each substring that was found and the number of times it was found.
---
+++ Substrings.rb
@@ -0,0 +1,29 @@
+def substrings(string, dictionary)
+  matches = {}
+  
+  # Creates a case array for the string input 
+  string_array = string.split(" ")
+
+  string_array.each { |string| 
+    dictionary.each { |substring|
+      # Case insensitive check for whether or not substring matches string 
+      if string.downcase.include? substring.downcase
+        # For the first entry, set as 1
+        if matches[substring] == nil
+          matches[substring] = 1
+        else
+          # For subsequent entries, add count
+          matches[substring] += 1
+        end
+      end
+    }
+  }
+  return matches
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+puts "Enter string:"
+string = gets.chomp
+
+substrings(string, dictionary)
